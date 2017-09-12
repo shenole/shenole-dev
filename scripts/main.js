c751bf3d1cec76ddf447005634a56c4e608dc2ea
Shenole Latimer <shenole@gmail.com>
2017-09-11 22:47:09 -0400

    added comment to main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,6 @@
+
+//Navigation - When burger nav is clicked, nav menu widens and burger nav changes to close icon.
+
 document.querySelector('.burger-nav').addEventListener('click', function() {
 	document.querySelector('#main-nav').classList.toggle('open');
 
