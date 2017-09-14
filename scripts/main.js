c45d4a16544aea1e902f253456ecfa07b3e47772
Shenole Latimer <shenole@gmail.com>
2017-09-14 14:21:43 -0400

    Completed swell animation

--- scripts/main.js
+++ scripts/main.js
@@ -6,3 +6,4 @@ document.querySelector('.burger-nav').addEventListener('click', function() {
 	document.querySelector('#main-nav').classList.toggle('open');
 	document.querySelector('.burger-nav').classList.toggle('close-it');
 });
+
