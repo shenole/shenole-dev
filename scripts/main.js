7613c7edc4dcba7fe694d8c01e54de3264edb5de
Shenole Latimer <shenole@gmail.com>
2017-09-14 17:08:04 -0400

    Completed navigation animation

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,7 @@
 - When burger nav is clicked, nav menu widens and burger nav changes to close icon. */
 
 document.querySelector('.burger-nav').addEventListener('click', function() {
-	document.querySelector('#main-nav').classList.toggle('open');
+	document.querySelector('nav').classList.toggle('open');
+	document.querySelector('#main-nav').classList.toggle('visible');
 	document.querySelector('.burger-nav').classList.toggle('close-it');
 });
