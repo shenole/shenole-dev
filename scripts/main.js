cee575de85060c09d6fc6ff1206d6d8d6c35b092
Shenole Latimer <shenole@gmail.com>
2017-09-16 16:35:13 -0400

    Javascript on click even added to project thumbs

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,28 @@ document.querySelector('.burger-nav').addEventListener('click', function() {
 	document.querySelector('.burger-nav').classList.toggle('close-it');
 });
 
+// Project panels are visible on click.
+
+var showProject = document.querySelectorAll('#lower-project-container .project-');
+
+Array.from(showProject).forEach(function(thumb) {
+	thumb.addEventListener('click', function(e) {
+		document.querySelector('#proj-' + e.target.id).style.display = 'block';
+	});
+});
+
+// If another project panel is visible, it is then hidden.
+
+var projectPanel = document.querySelectorAll('.project-text-left');
+
+/*Array.from(projectPanel).forEach(function(e) {
+	console.log(projectPanel);
+});*/
+
+
+
+
+	if (projectPanel.id != panel.id) {
+		projectPanel.style.display = 'none';
+	}
+//});
