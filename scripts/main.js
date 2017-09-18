3e8e96662e42d3f704996cf1b879147ec681308c
Shenole Latimer <shenole@gmail.com>
2017-09-17 22:57:16 -0400

    Finished JS for selecting projects

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 
-/* NAVIGATION
-- When burger nav is clicked, nav menu widens and burger nav changes to close icon. */
+// ----- NAVIGATION
+// When burger nav is clicked, nav menu widens and burger nav changes to close icon. */
 
 document.querySelector('.burger-nav').addEventListener('click', function() {
 	document.querySelector('nav').classList.toggle('open');
@@ -8,28 +8,22 @@ document.querySelector('.burger-nav').addEventListener('click', function() {
 	document.querySelector('.burger-nav').classList.toggle('close-it');
 });
 
-// Project panels are visible on click.
+// ----- PROJECT PAGE
+/*Project panels are visible on click, but 
+If another project panel is visible, it is then hidden.*/
 
 var showProject = document.querySelectorAll('#lower-project-container .project-');
+var siteDescr = document.querySelectorAll('.project-text-left');
+var oldID = null;
 
-Array.from(showProject).forEach(function(thumb) {
-	thumb.addEventListener('click', function(e) {
-		document.querySelector('#proj-' + e.target.id).style.display = 'block';
-	});
+(showProject).forEach(function(thumb) {
+    thumb.addEventListener('click', function(e) {
+        const project = document.querySelector('#proj-' + e.target.id);
+        if( oldID != null ) {
+           document.querySelector('#proj-'+ oldID).style.display = "none";
+        }
+        project.style.display = 'block';
+        //--- save current id
+        oldID = e.target.id;
+    });
 });
-
-// If another project panel is visible, it is then hidden.
-
-var projectPanel = document.querySelectorAll('.project-text-left');
-
-/*Array.from(projectPanel).forEach(function(e) {
-	console.log(projectPanel);
-});*/
-
-
-
-
-	if (projectPanel.id != panel.id) {
-		projectPanel.style.display = 'none';
-	}
-//});
